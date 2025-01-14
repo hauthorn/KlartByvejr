374bb82cff710b93698ace91ca38322fce263f51
Christoffer Berg Boisen <christoffer@emplate.it>
2025-01-14 21:25:36 +0100

    Load the image into the main layout using Glide

--- app/src/main/java/me/hauthorn/byvejr/MainActivity.kt
+++ app/src/main/java/me/hauthorn/byvejr/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Glide.with(this)
+            .load("https://www.klartvejr.dk/kort/7500")
+            .into(findViewById(R.id.weather_view))
     }
 }
